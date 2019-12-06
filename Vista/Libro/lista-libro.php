b5588d5598c295e4a10857d3517ba20a9577a4a5
rayleight20 <alexx.rodrigo.20@gmail.com>
2019-12-06 06:22:35 -0500

    adwad

--- Vista/Libro/lista-libro.php
+++ Vista/Libro/lista-libro.php
@@ -4,10 +4,45 @@
 <?php include("head.php"); ?>
 
 <body>
+<div id="wrapper">
 
-<p>hola</p>
-    
+        <?php include("panel.php"); ?>      
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <br>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Libros Registrados
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="dataTable_wrapper">
+                                
+                            </div>
+                            <!-- /.table-responsive -->
+                            
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            
+        </div>
+        <!-- /#page-wrapper -->
 
+    </div>
+    <!-- /#wrapper -->
+<?php include("scripts.php"); ?>
 </body>
 
 </html>
